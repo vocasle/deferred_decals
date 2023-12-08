bf3bb25204e0892b203833eb0211dce8f9233206
Nikita Elsakov <vocasle@gmail.com>
2023-12-08 21:31:04 +0200

    Add Transform

--- src/decals.c
+++ src/decals.c
@@ -33,6 +33,12 @@
 	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 #endif
 
+struct Transform {
+	Vec3D translation;
+	Vec3D rotation;
+	Vec3D scale;
+};
+
 struct File {
 	char *contents;
 	uint64_t size;
@@ -600,37 +606,13 @@ int main(void)
 		{
 			PushRenderPassAnnotation("Nuklear Pass");
 			struct nk_context* ctx = &game.nuklear.ctx;
-			if (nk_begin(ctx, "Demo", nk_rect(50, 50, 230, 250),
+			if (nk_begin(ctx, "Options", nk_rect(50, 50, 230, 250),
 				NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE |
 				NK_WINDOW_MINIMIZABLE | NK_WINDOW_TITLE))
 			{
-				enum { EASY, HARD };
-				static int op = EASY;
-				static int property = 20;
-				nk_layout_row_static(ctx, 30, 80, 1);
-				if (nk_button_label(ctx, "button"))
-					fprintf(stdout, "button pressed\n");
-
-				nk_layout_row_dynamic(ctx, 30, 2);
-				if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
-				if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
-
-				nk_layout_row_dynamic(ctx, 25, 1);
-				nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-				nk_layout_row_dynamic(ctx, 20, 1);
-				nk_label(ctx, "background:", NK_TEXT_LEFT);
-				nk_layout_row_dynamic(ctx, 25, 1);
-				if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx), 400))) {
-					nk_layout_row_dynamic(ctx, 120, 1);
-					bg = nk_color_picker(ctx, bg, NK_RGBA);
-					nk_layout_row_dynamic(ctx, 25, 1);
-					bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f, 0.005f);
-					bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f, 0.005f);
-					bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f, 0.005f);
-					bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f, 0.005f);
-					nk_combo_end(ctx);
-				}
+				nk_layout_row_static(ctx, 30, 180, 1);
+				nk_label(ctx, "Decal 1 transform:", NK_TEXT_ALIGN_LEFT);
+				
 			}
 			nk_end(ctx);
 
@@ -1317,4 +1299,14 @@ void PushRenderPassAnnotation(const char* passName)
 void PopRenderPassAnnotation(void)
 {
 	glPopDebugGroup();
+}
+
+Mat4X4 TransformToMat4X4(const struct Transform *t)
+{
+	const Mat4X4 translation = MathMat4X4TranslateFromVec3D(&t->translation);
+	const Mat4X4 rotation = MathMat4X4RotateFromVec3D(&t->rotation);
+	const Mat4X4 scale = MathMat4X4ScaleFromVec3D(&t->scale);
+	Mat4X4 ret = MathMat4X4MultMat4X4ByMat4X4(&translation, &rotation);
+	ret = MathMat4X4MultMat4X4ByMat4X4(&ret, &scale);
+	return ret;
 }
