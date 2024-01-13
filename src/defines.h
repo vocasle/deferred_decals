78cf05dc3d9d54e21ebe0e7461272734f871bfa3
Nikita Elsakov <vocasle@gmail.com>
2024-01-13 10:57:57 +0200

    Add defines

---
+++ src/defines.h
@@ -0,0 +1,20 @@
+#pragma once
+
+typedef unsigned char u8;
+typedef char i8;
+
+typedef unsigned short u16;
+typedef short i16;
+
+typedef unsigned int u32;
+typedef int i32;
+
+typedef unsigned long long u64;
+typedef long long i64;
+
+typedef float f32;
+typedef double f64;
+
+#define TRUE 1
+#define FALSE 0
+typedef unsigned char boolean;
